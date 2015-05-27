08e34ba89c9d778a56fe2088af785ed5f07e35fc
katjam <katja@neontribe.co.uk>
2015-05-27 16:09:23 +0100

    testing deploy to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,21 @@ echo -e "Pull req"
 echo -e "$TRAVIS_PULL_REQUEST"
 echo -e "travis:"
 echo -e "$TRAVIS"
+
+#if this isn't a pull req set default github token
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+    if [ "$TRAVIS" == "true" ]; then
+        git config --global user.email "katja@neontribe.co.uk"
+        git config --global user.name "katjam"
+    fi
 echo -e "Git Hub token"
 echo -e "$GH_TOKEN"
+
+#checkout gh_pages branch and update with contents of build folder
+git checkout gh-pages
+cp build/* .
+git add .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
+git push origin gh-pages
+fi
