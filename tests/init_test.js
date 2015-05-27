dda28c952a6f4af32944043d67cf35a7031dc310
katjam <katja@neontribe.co.uk>
2015-05-27 14:16:35 +0100

    added url tests to init

--- tests/init_test.js
+++ tests/init_test.js
@@ -1,6 +1,7 @@
-casper.test.begin('Title page', 1, func,tion suite(test) {
+casper.test.begin('Title page', 3, function suite(test) {
     casper.start("http://localhost:9001/build", function() {
-      console.log('hello');
+      this.test.comment(this.getCurrentUrl());
+      this.test.assertHttpStatus(200, siteName + ' is up');
       test.assertTitle("My ESA Assessment", "ESA page title is the one expected");
         //test.assertExists('form[action="/search"]', "main form is found");
         //this.fill('form[action="/search"]', {
