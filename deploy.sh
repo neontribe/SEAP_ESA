1f5fca4cd5f7cfd4f8d2c80e089d835061d75e7a
katjam <katja@neontribe.co.uk>
2015-05-27 15:48:32 +0100

    Deploy debug

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 ##!/usr/bin/env bash
 echo -e "Testing deploy"
+
+echo -e "$VARNAME"
+echo -e "Pull req"
+echo -e "$TRAVIS_PULL_REQUEST"
+echo -e "travis:"
+echo -e "$TRAVIS"
+echo -e "Git Hub token"
+echo -e "$GH_TOKEN"
