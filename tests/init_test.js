a43f16f9d766845f2e5d2f06bf3b18cd8f56c9d3
katjam <katja@neontribe.co.uk>
2015-05-27 14:11:08 +0100

    A nice greeting

--- tests/init_test.js
+++ tests/init_test.js
@@ -1,6 +1,6 @@
 casper.test.begin('Title page', 1, func,tion suite(test) {
     casper.start("http://localhost:9001/build", function() {
-      console.log(this.title);
+      console.log('hello');
       test.assertTitle("My ESA Assessment", "ESA page title is the one expected");
         //test.assertExists('form[action="/search"]', "main form is found");
         //this.fill('form[action="/search"]', {
