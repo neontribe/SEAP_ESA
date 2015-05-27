066bcebf280c43216dd6e918f1438350157d22e3
katjam <katja@neontribe.co.uk>
2015-05-27 13:12:40 +0100

    Add localhost port as test url base

--- tests/init_test.js
+++ tests/init_test.js
@@ -1,5 +1,5 @@
 casper.test.begin('Title page', 1, function suite(test) {
-    casper.start("http://seap-app", function() {
+    casper.start("http://localhost:9001/build", function() {
         test.assertTitle("My ESA Assessment", "ESA page title is the one expected");
         //test.assertExists('form[action="/search"]', "main form is found");
         //this.fill('form[action="/search"]', {
