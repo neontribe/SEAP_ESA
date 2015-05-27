dbc6f622fa572241c09a5f0dc217b0cc7ac24e47
katjam <katja@neontribe.co.uk>
2015-05-27 17:32:27 +0100

    change remote before cp and commit

--- deploy.sh
+++ deploy.sh
@@ -18,13 +18,13 @@ echo -e "Git Hub token"
 echo -e "$GH_TOKEN"
 
 #checkout gh_pages branch and update with contents of build folder
+git remote rm origin
+git remote add origin https://katjam:9e0e2d43e5b1c07ee6da44d25e46840795da1e9f@github.com/neontribe/SEAP_ESA.git
 git fetch
 git checkout gh-pages
 cp -r build/* .
 git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-git remote rm origin
-git remote add origin https://katjam:9e0e2d43e5b1c07ee6da44d25e46840795da1e9f@github.com/neontribe/SEAP_ESA.git
 git pull origin gh-pages
 git push origin gh-pages
 fi
