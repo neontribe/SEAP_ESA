c6fc21f67a43878315dd886300b0f7dc70459ca9
katjam <katja@neontribe.co.uk>
2015-05-27 14:19:58 +0100

    Added some comment shite

--- tests/init_test.js
+++ tests/init_test.js
@@ -1,7 +1,7 @@
 casper.test.begin('Title page', 3, function suite(test) {
     casper.start("http://localhost:9001/build", function() {
-      this.test.comment(this.getCurrentUrl());
-      this.test.assertHttpStatus(200, siteName + ' is up');
+      this.test.comment('bum'+this.getCurrentUrl());
+      this.test.assertHttpStatus(200, 'SEAP is up');
       test.assertTitle("My ESA Assessment", "ESA page title is the one expected");
         //test.assertExists('form[action="/search"]', "main form is found");
         //this.fill('form[action="/search"]', {
