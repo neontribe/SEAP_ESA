ab8c9d431781a109b3cc4e22ed6c05f9fd95a8ea
katjam <katja@neontribe.co.uk>
2015-07-01 11:07:12 +0000

    Travis build 126 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14144,6 +14144,11 @@ function initAss() {
 		unseenQuestions: [],
 		seenQuestions: [],
 		skippedQuestions: [], // the questions which have been viewed but not answered
+		allCategories: _.uniq(_.reject(window.allCategories, 
+			function(cat) { 
+				return cat && cat.charAt(0) === '*'; 
+			}
+		)),
 		remainingCategories: _.uniq(_.reject(window.allCategories, 
 			function(cat) { 
 				return cat && cat.charAt(0) === '*'; 
@@ -14569,6 +14574,31 @@ function compileStats() {
 
 }
 
+function disabledCats() {
+
+	var remaining = db.get('ass.remainingCategories');
+
+	$('.real-cat').each(function() {
+
+		var button = $('button', this);
+
+		button.attr('disabled', null);
+
+		var catName = button.attr('data-category');
+
+		console.log('remaining', remaining);
+		console.log('disabled?', !_.contains(remaining, catName));
+
+		if (!_.contains(remaining, catName)) {
+
+			button.attr('disabled', 'disabled');
+			
+		}
+
+	});
+
+}
+
 function compileCategories() {
 
 	// template up the stats with handlebars and 
@@ -14577,6 +14607,9 @@ function compileCategories() {
 	var assData = db.get('ass');
 	var output = template(assData);
 	$('#categories-content').html(output);
+
+	// disable seen categories
+	disabledCats();
 
 }
 
