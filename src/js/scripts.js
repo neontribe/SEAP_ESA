2479318298ba10c069bf2bdc748a2b7ffac783ba
katjam <katja@neontribe.co.uk>
2015-07-31 16:48:58 +0100

    Catch seen all even skipped

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -255,6 +255,10 @@ function pickQuestion() {
 			}
 		}
 	} else {
+		if (mode === 'unseenQuestions' && _.isEmpty(db.get('ass.unseenQuestions')) && _.isEmpty(db.get('ass.skippedQuestions'))) {
+			loadSlide('seen-all-even-skipped');
+			return;
+		}
 		if (mode === 'unseenQuestions' && _.isEmpty(db.get('ass.unseenQuestions'))) {
 			loadSlide('seen-all');
 			return;
