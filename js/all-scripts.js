2c0b91985fc06a9bf267dcf589aa6ff8e1ca3687
katjam <katja@neontribe.co.uk>
2015-08-11 17:00:37 +0000

    Travis build 189 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14589,8 +14589,8 @@ function disabledCats() {
 
 		var catName = button.attr('data-category');
 
-		console.log('remaining', remaining);
-		console.log('disabled?', !_.contains(remaining, catName));
+		// console.log('remaining', remaining);
+		// console.log('disabled?', !_.contains(remaining, catName));
 
 		if (!_.contains(remaining, catName)) {
 
