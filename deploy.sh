f89558887ef6b284c9c190ede2474b12f8d30afa
katjam <katja@neontribe.co.uk>
2015-08-24 13:31:47 +0100

    Add in live deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,17 @@
 ##!/usr/bin/env bash
-echo -e "Testing deploy"
-
-echo -e "$VARNAME"
-echo -e "Pull req"
-echo -e "$TRAVIS_PULL_REQUEST"
-echo -e "travis:"
-echo -e "$TRAVIS"
+echo -e "Tests passed - deploying to STAGING"
 
 #if this isn't a pull req set default github token
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "katja@neontribe.co.uk"
-        git config --global user.name "katjam"
+        git config --global user.email $GIT_EMAIL
+        git config --global user.name $GIT_USER
     fi
-echo -e "Git Hub token"
-echo -e "$GH_TOKEN"
 
 #checkout gh_pages branch and update with contents of build folder
 git remote rm origin
-git remote add origin https://katjam:9e0e2d43e5b1c07ee6da44d25e46840795da1e9f@github.com/neontribe/SEAP_ESA.git
+git remote add origin https://$GIT_USER:$GIT_PASS@github.com/neontribe/SEAP_ESA.git
 git fetch
 git checkout gh-pages
 cp -r build/* .
@@ -29,4 +21,28 @@ git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
 git pull origin gh-pages
 git push origin gh-pages
+fi
+
+#if this is a tagged release, deploy to LIVE
+if [ "$TRAVIS_TAG" ]; then
+    echo -e "Release tag:"
+    echo -e $TRAVIS_TAG
+    DATETIME=`date +%Y%m%d"-"%H%M%S`
+    echo -e "Prepare files for live deploy"
+    #Deploy gh-pages to deployment server
+    cd ..
+    tar -czf release.tgz SEAP_ESA 
+    sudo apt-get -y install sshpass
+
+    #copy tarball to deployment server
+    sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no release.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+
+    echo -e "Deploying to Live."
+
+    #ssh onto deployment server. unpack. tidy. fire live deploy script.
+    sshpass -p $DEPLOY_PASS ssh $DEPLOY_USER@$DEPLOY_HOST  "cd $DEPLOY_PATH && tar -xvf release.tgz && rm -rf SEAP_ESA/.git && printf "$TRAVIS_TAG-$DATETIME" > SEAP_ESA/release.txt && rm release.tgz && bash deploy-seap-esa.sh"
+
+    #TODO need to add robots.txt for live in above script or in deploy-seap-esa.sn on deploy server
+
+    echo -e "SUCCESS: Released tag - $TRAVIS_TAG at $DATETIME"
 fi
