d3748fac6943ddff65e89ec1af71309359f524ce
katjam <katja@neontribe.co.uk>
2015-11-13 10:52:30 +0000

    Travis build 424 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14134,6 +14134,15 @@ if (db.isEmpty('esaAss')) {
 
 }
 
+$(function() {
+   var isMobile = window.matchMedia("only screen and (max-width: 800px)");
+
+   if (isMobile.matches) {
+       $("#about-esa .expandies button").attr("aria-expanded", "false");
+       $("#about-esa .expandies h2").next().attr("aria-hidden", "true");
+   }
+});
+
 /**********************************************************************
 FUNCTIONS
 **********************************************************************/
@@ -14228,6 +14237,10 @@ function loadSlide(id, type) {
 
   }
 
+  if (id === 'about-esa') {
+    compileAboutButtons();
+  }
+
   if (id === 'categories') {
     compileCategories();
   }
@@ -14259,7 +14272,7 @@ function loadSlide(id, type) {
     .focus();
 
   // find out if we've gone to one of the locations that don't need saving
-  var exclude = _.find(['resume', 'are-you-sure', 'deleted', 'break-time',],
+  var exclude = _.find(['resume', 'break-time', 'resume-practise'],
     function(unsaveable) {
       return unsaveable === id;
     });
@@ -14591,6 +14604,13 @@ function disabledCats() {
 
 }
 
+function compileAboutButtons() {
+  var template = Handlebars.compile(document.getElementById("about-buttons-template").innerHTML);
+  var esaAssData = db.get('esaAss');
+  var output = template(esaAssData);
+  $('.expandies.information .about-buttons-content').html(output);
+}
+
 function compileCategories() {
 
   // template up the stats with handlebars and
@@ -14851,13 +14871,6 @@ $('body').on('click', '[data-action="clean-up"]', function() {
 
 });
 
-$('body').on('click', '[data-action="delete-are-you-sure"]', function() {
-
-  // load the deleted data slide
-  loadSlide('are-you-sure');
-
-});
-
 $('body').on('click', '[data-action="delete-data"]', function() {
 
   // set answered global to false
@@ -14905,9 +14918,6 @@ $('body').on('change', '[data-action="save-basic-info"]', function() {
 });
 
 $('body').on('change', '[type="radio"]', function() {
-
-  // add highlighted class to pick button when an answer button is pressed
-  $('.loaded button.nav-link[data-action="pick"]').addClass( 'highlighted' );
 
   // record that change has been made
   window.answered = true;
