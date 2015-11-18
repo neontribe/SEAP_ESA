b76f4a1fc9459c0371c24b2db2848c2ecddf02f1
katjam <katja@neontribe.co.uk>
2015-11-18 10:16:46 +0000

    Travis build 476 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14218,11 +14218,18 @@ function getCatQuestionArr(slug) {
 
 function loadSlide(id, type) {
 
+  // Register google page view
+  var trackHashes = ['main-menu', 'stats', 'data', 'about-esa', 'start'];
+
+  if ($.inArray(id, trackHashes) !== -1) {
+    ga('send', 'pageview', '#' + id);
+  }
+
   // Oops! we got here without an id to load - probably resuming user
   // session after data deleted. So no esaAss.whereIAm defined but computer
   // thinks user has been here before.
-  if (!id) {
-    id = 'main-menu';
+  if (!id || id === 'undefined') {
+    loadSlide('main-menu');
   }
 
   if (id === 'stats') {
@@ -15040,36 +15047,4 @@ $(window).on('hashchange', function(e) {
     }
   }
 
-});
-
-// Which c-app site are we?
-var siteTitle = $(document).find("title").text();
-    site = 'c-app';
-    if (siteTitle === 'ESA Assessment Support') {
-      site = 'esa';
-    }
-    if (siteTitle === 'PIP Assessment Support') {
-      site = 'pip';
-    }
-
-var siteAss = site + 'Ass';
-
-// Track all slides as pageviews
-$(window).on('hashchange', function(e) {
-  // @todo add list of # not to track. For now let's call every change a page.
-  var page = window.location.hash;
-  ga('send', 'pageview', page);
-});
-
-// Event on every a
-$('body').on('click', 'a', function(e) {
-  var page = '';
-      linkText = $(this).text();
-  // Attempt to get the page we came from in the hashHistory.
-  // If that fails, call it unknown.
-  page = _.last(window.hashHistory);
-  if (!page) {
-    page = 'unknown';
-  }
-  ga('send', 'event', page, 'a-link-click', linkText, null);
 });
