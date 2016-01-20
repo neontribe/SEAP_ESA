b63b1f2dc96625483de53e5ea11b4e8b2d60c70f
katjam <katjamordaunt@gmail.com>
2016-01-20 17:38:57 +0000

    corrected comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -338,7 +338,7 @@ function pickQuestion() {
 
 }
 
-// clear data and go to start screen
+// clear data and go to chose an activity screen
 function restart() {
 
   db.set('esaAss.unseenQuestions', []);
@@ -349,7 +349,7 @@ function restart() {
   db.set('esaAss.category', null);
   db.set('esaAss.remainingCategories', _.uniq(window.allCategories));
 
-  // go to start screen
+  // go to chose activity screen
   loadSlide('categories');
 
 }
