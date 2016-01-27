c4c112fc32d5b5448cc8a3da40c5fa98aae54eff
tomltd <tomfreestone@gmail.com>
2016-01-27 13:34:39 +0000

    it worked the first way

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 $(function() {
-    if ( window.location.href.indexOf("neontribe.github.io") > -1 ) {
+    if ( document.location.href.indexOf('neontribe.github.io') > -1 ) {
         console.log('hi');
     }
 });
