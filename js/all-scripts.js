f03a528aafec7120140c820652542d90a9b55342
katjam <katja@neontribe.co.uk>
2016-01-28 13:47:58 +0000

    Travis build 698 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -15474,12 +15474,12 @@ $(function() {
     var buttonData = $(this);
     if (buttonData.text() === buttonData.data("text-swap")) {
       buttonData.text(buttonData.data("text-original"));
-      $( ".video-embed").html("<iframe id='video-iframe' src='https://player.vimeo.com/video/145264947?api=1&player_id=video-iframe' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
+      $( ".video-embed").html("<iframe id='video-iframe' src='https://player.vimeo.com/video/145264947' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
       getPlayer();
     } else {
       buttonData.data("text-original", buttonData.text());
       buttonData.text(buttonData.data("text-swap"));
-      $( ".video-embed").html("<iframe id='video-iframe' src='https://player.vimeo.com/video/139480207?api=1&player_id=video-iframe' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
+      $( ".video-embed").html("<iframe id='video-iframe' src='https://player.vimeo.com/video/139480207' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
       getPlayer();
     }
   });
