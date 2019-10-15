a1e1f3b17fec5760f92d6a6d6b8633ab652f9ba7
Alin <alin@neontribe.co.uk>
2019-10-15 15:32:55 +0100

    Fix config file with name of new dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt){
 
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      htmlhint: {
+      htmlhintplus: {
         build: {
             options: {
               rules: {
@@ -127,7 +127,7 @@ module.exports = function(grunt){
       }
     });
 
-    grunt.registerTask('generate', ['clean:initial', 'copy','compile-handlebars','bake', 'htmlhint', 'jshint', 'concat', 'autoprefixer', 'clean:tidyup']);
+    grunt.registerTask('generate', ['clean:initial', 'copy','compile-handlebars','bake', 'htmlhintplus', 'jshint', 'concat', 'autoprefixer', 'clean:tidyup']);
     grunt.registerTask('test', ['generate', 'connect', 'casperjs']);
-    grunt.registerTask('generate-production', ['clean:initial','compile-handlebars', 'htmlhint', 'jshint', 'copy', 'concat', 'uglify', 'autoprefixer','cssmin', 'clean:tidyup']);
+    grunt.registerTask('generate-production', ['clean:initial','compile-handlebars', 'htmlhintplus', 'jshint', 'copy', 'concat', 'uglify', 'autoprefixer','cssmin', 'clean:tidyup']);
 };
