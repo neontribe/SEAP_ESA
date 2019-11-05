c697cb58e375d1c0f8145e8c05dd298dc130253c
Alin <alin@neontribe.co.uk>
2019-11-05 17:30:12 +0000

    Fix one more test. 2/5 are working now

--- tests/data_test.js
+++ tests/data_test.js
@@ -3,8 +3,10 @@
 
   url = 'http://localhost:9001/build';
 
-  casper.test.begin("Your Assessment Page", 1, function(test) {
-    return casper.start(url, function() {
+  casper.test.begin("Your Assessment Page", 2, function(test) {
+    return casper
+      .start(url, function() {
+        test.assertHttpStatus(200);
       return this.click('a[data-action="your-assessment"]');
     }).then(function() {
       return test.assertUrlMatch(url + '/#your-assessment', 'press link navigates to your-assessment page');
